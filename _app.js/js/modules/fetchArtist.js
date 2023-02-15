0c2e23426dd80fe26d5dff11403c83eeac05d9af
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-02-15 13:30:27 +0100

    Started working on asynch function

--- _app.js/js/modules/fetchArtist.js
+++ _app.js/js/modules/fetchArtist.js
@@ -1,3 +1,6 @@
 export default async function fetchArtist() {
-	
+	const endpointArtist = '';
+
+	const responses = await Promise.fetch(endpointArtist);
+	const result = await Promise.responses.map(response => response.json());
 }
