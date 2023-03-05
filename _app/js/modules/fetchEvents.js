ba1873b5214e0477b634f3a4251a15f404921022
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-03-05 10:07:10 +0100

    Added ticketmaster endpoint for Berlin events

--- _app/js/modules/fetchEvents.js
+++ _app/js/modules/fetchEvents.js
@@ -1,5 +1,5 @@
 export default async function fetchEvents() {
-	const endpointEvents = '';
+	const endpointEvents = 'https://app.ticketmaster.com/discovery/v2/events?apikey=3PgslbneI8EdQweGEK0RAM0Ii9iCL3Rj&locale=*&city=Berlin&countryCode=DE';
 
 	const responses = await Promise.fetch(endpointEvents);
 	const result = await Promise.responses.map(response => response.json());
