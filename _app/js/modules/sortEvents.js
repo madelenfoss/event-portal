36bd2100a4ad036f1961c2ed23be6bd2ac65362c
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-03-08 13:47:56 +0100

    Created new sortEvents module + added comments

---
+++ _app/js/modules/sortEvents.js
@@ -0,0 +1,8 @@
+
+
+// All: data.classifications[0].segment.name
+// Music: data.classifications[0].segment.name === 'Music'
+// Sport: data.classifications[0].segment.name === 'Sports'
+// Music: data.classifications[0].segment.name === 'Arts & Theatre'
+// Music: data.classifications[0].segment.name === 'Comedy'
+// Music: data.classifications[0].segment.name === 'Miscellaneous'
