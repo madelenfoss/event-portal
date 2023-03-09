ab8b9f079c11702ebe626d891713acfaeb7a6c22
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-03-09 21:05:16 +0100

    Started on new code for berlinEvents

--- _app/js/modules/renderList.js
+++ _app/js/modules/renderList.js
@@ -9,19 +9,98 @@ export default function renderList(berlinEvents) {
   				<img class="events__img" src="${berlinEvent.image}" alt="event image">
   				<div class="events__info">
   					<div class="events__date">${berlinEvent.date}</div>
-  					<h3 class="events__name">${berlinEvent.eventname}</h3>
-  					<div><span class="events__genre">${berlinEvent.genre}</span>
-					<span> - </span>
-					<span class="events__venue">${berlinEvent.venue}</span></div>
+  					<h3 class="events__name">${berlinEvent.eventName}</h3>
+					<div class="events__genre">${berlinEvent.genre}</div>
+					<div class="events__venue">${berlinEvent.venue}</div>
   				</div>
-  				<a target="_blank" href="${berlinEvent.tickets}"><button class="events__tickets">Tickets</button><a/>
+  				<a class="events__tickets" target="_blank" href="${berlinEvent.tickets}">
+				Tickets
+				<a/>
 		`
 		berlinEventsElement.appendChild(eventElement);
+
+
+		// const eventImage = berlinEvent.image;
+		// const eventDate = berlinEvent.date;
+		// const eventName = berlinEvent.eventname;
+		// const eventGenre = berlinEvent.genre;
+		// const eventVenue = berlinEvent.venue;
+		// const eventTicket = berlinEvent.tickets;
+
+		// const eventElement = document.createElement('div');
+		// const imageElement = document.createElement('img');
+		// const infoElement = document.createElement('div');
+		// const dateElement = document.createElement('div');
+		// const nameElement = document.createElement('h3');
+		// const genreElement = document.createElement('div');
+		// const venueElement = document.createElement('div');
+		// const ticketElement = document.createElement('a');
+
+		// eventElement.classList.add('events__event');
+		// imageElement.classList.add('events__img');
+		// infoElement.classList.add('events__info');
+		// dateElement.classList.add('events__date');
+		// nameElement.classList.add('events__name');
+		// genreElement.classList.add('events__genre');
+		// venueElement.classList.add('events__venue');
+		// ticketElement.classList.add('events__tickets');
+
+		// imageElement.setAttribute('src', eventImage
+		//		.find(image=> image.width > 600)?.url);
+		// dateElement.textContent = `${eventDate}`;
+		// nameElement.textContent = `${eventName}`;
+		// genreElement.textContent = `${eventGenre}`;
+		// venueElement.textContent = `${eventVenue}`;
+		// ticketElement.textContent = `${eventTicket}`;
+
+		// berlinEventsElement.appendChild(eventElement);
+		// eventElement.append(imageElement, infoElement);
+		// infoElement.append(eventDate, eventName, eventGenre, eventVenue);
+		// berlinEventsElement.appendChild(eventTicket);
+
 	})
 
 	 console.log(berlinEvents);
 } 
 
+// const eventImage = berlinEvent.image;
+// const eventDate = berlinEvent.date;
+// const eventName = berlinEvent.eventname;
+// const eventGenre = berlinEvent.genre;
+// const eventVenue = berlinEvent.venue;
+// const eventTicket = berlinEvent.tickets;
+
+// const eventElement = document.createElement('div');
+// eventElement.classList.add('events__event');
+
+// const imageElement = document.createElement('img');
+// imageElement.classList.add('events__img');
+// imageElement.setAttribute('src', eventImage.find(image=> image.width > 600)?.url);
+
+// const infoElement = document.createElement('div');
+// infoElement.classList.add('events__info');
+
+// const dateElement = document.createElement('div');
+// dateElement.classList.add('events__date');
+// dateElement.textContent = `${eventDate}`;
+
+// const nameElement = document.createElement('h3');
+// nameElement.classList.add('events__name');
+// nameElement.textContent = `${eventName}`;
+
+// const genreElement = document.createElement('div');
+// genreElement.classList.add('events__genre');
+// genreElement.textContent = `${eventGenre}`;
+
+// const venueElement = document.createElement('div');
+// venueElement.classList.add('events__venue');
+// venueElement.textContent = `${eventVenue}`;
+
+
+// berlinEventsElement.appendChild(eventElement);
+// eventElement.append(imageElement, infoElement);
+// infoElement.append(eventDate, eventName, eventGenre, eventVenue);
+// berlinEventsElement.appendChild(eventTicket);
 
 // Lage en function?
 		// const eventImage = berlinEvent.image;
