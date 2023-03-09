508dfc92fc95f49ddb9523fc91866c7c7596c09c
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-03-09 13:29:56 +0100

    Created filterEvents module, imported to main

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,8 +1,10 @@
 import fetchEvents from "./modules/fetchEvents.js";
 import renderList from "./modules/renderList.js";
 import backToTop from "./modules/toTopButton.js";
+import filterEvents from "./modules/filterEvents.js";
 
 backToTop();
 renderList(await fetchEvents());
+// filterEvents();
 
 
--- _app/js/modules/sortEvents.js
+++
@@ -1,7 +0,0 @@
-
-// All: data.classifications[0].segment.name
-// Music: data.classifications[0].segment.name === 'Music'
-// Sport: data.classifications[0].segment.name === 'Sports'
-// Music: data.classifications[0].segment.name === 'Arts & Theatre'
-// Music: data.classifications[0].segment.name === 'Comedy'
-// Music: data.classifications[0].segment.name === 'Miscellaneous'
