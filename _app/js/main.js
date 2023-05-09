184e79edf50e02f7723ab72fa199c8c05de37a06
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-09 15:22:29 +0200

    Imported and called filterEvents in main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,10 +2,12 @@ import fetchEvents from "./modules/fetchEvents.js";
 import renderList from "./modules/renderList.js";
 import backToTop from "./modules/toTopButton.js";
 import Navigation from "./modules/toggleMenu.js";
+import filterEvents from "./modules/filterEvents.js";
 
 Navigation();
 backToTop();
 renderList(await fetchEvents());
+filterEvents();
 
 
 
