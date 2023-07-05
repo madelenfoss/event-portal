6ba8b9a6165205ea8c9d357c1ee80532408e4e36
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-07-05 13:15:23 +0200

    Removed env import from main

--- _app/js/main.js
+++ _app/js/main.js
@@ -3,13 +3,11 @@ import renderList from "./modules/renderList.js";
 import backToTop from "./modules/toTopButton.js";
 import Navigation from "./modules/toggleMenu.js";
 import filterEvents from "./modules/filterEvents.js";
-import env from "./env.js";
 
 Navigation();
 backToTop();
 renderList(await fetchEvents());
 filterEvents();
-env();
 
 
 
