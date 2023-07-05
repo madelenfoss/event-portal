f9fe49667827ab9487d2301b3e29cb99f67a7b2f
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-07-05 13:14:08 +0200

    Testing env file

--- _app/js/main.js
+++ _app/js/main.js
@@ -3,11 +3,13 @@ import renderList from "./modules/renderList.js";
 import backToTop from "./modules/toTopButton.js";
 import Navigation from "./modules/toggleMenu.js";
 import filterEvents from "./modules/filterEvents.js";
+import env from "./env.js";
 
 Navigation();
 backToTop();
 renderList(await fetchEvents());
 filterEvents();
+env();
 
 
 
